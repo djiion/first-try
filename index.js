e9ccacdd85d6665fb2d19b7e3357b6ee0ef98134
djiion <djiion@yandex.ru>
2023-12-09 22:47:21 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// Переменные и типы данных
+
+
+/*
+let firstNumber;
+let secondNumber;
+let result;
+
+firstNumber = 44;
+console.log(firstNumber)
+
+secondNumber = 55 - 31;
+console.log(secondNumber)
+
+
+result = firstNumber + secondNumber
+
+
+console.log(secondNumber - 10)
+console.log(firstNumber + 10)
+console.log(firstNumber + secondNumber)
+console.log(firstNumber / secondNumber)
+console.log(firstNumber * secondNumber)
+console.log(`Результатом выражения ${firstNumber} + ${secondNumber} будет: ${result} `)
+
+
+
+let firstName = "Evgeny";
+let userAge = 5;
+let isMarried = true;
+console.log(`Меня зовут ${firstName}, мне ${userAge} лет. Я женат. `)
+*/
+
+/*
+let age = 25;
+let price = 10.99;
+let gpa = 3.1;
+
+console.log(typeof gpa)
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`You gpa is: ${gpa}`);
+
+
+
+let firstName = "Bro";
+let favoriteFood = "pizza";
+let email = "Bro123@gmail.com"
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favoriteFood}`);
+console.log(`You email ${email}`);
+
+
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(`Bro is online: ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
+*/
+
+
+let fullName = "Bro Code";
+let age = 25;
+let isStudent = false;
+
+
+document.querySelector(".p1").textContent = `Your name is ${fullName}`;
+document.querySelector(".p2").textContent = `You are ${age} years old`;
+document.querySelector(".p3").textContent = `Enrolled: ${isStudent}`;
